c05e80b2cb35a49afa1e05a2d0f344d9877885b2
Michelle Osorio <osoriomica@gmail.com>
2025-04-19 13:21:12 +0100

    add secret_key

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-e8l93d)j#)7k$!rr*sog-pcuo(w#4o8-v^*qply^0prgcx*-tw'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False

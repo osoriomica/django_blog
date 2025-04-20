a73484d7c5b524e7d00f1621407aef804c9bd4bf
Michelle Osorio <osoriomica@gmail.com>
2025-04-20 20:30:18 +0100

    Add queryset to views.py

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,6 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     # model = Post -now redundant by queryset
-    queryset = Post.objects.all()
-    template_name = 'post_list.html'
+    queryset = Post.objects.filter(status=1)
+    template_name = 'post_list.html' 
+    # above line is redundant by django's default naming rule which is <app_name>/<model_name>_list.html

01e0b6aeb2fad97ab88d9aea032026b05b42bb07
Michelle Osorio <osoriomica@gmail.com>
2025-04-30 00:30:20 +0100

    Add form to about page

--- blog/views.py
+++ blog/views.py
@@ -34,6 +34,7 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
 
     if request.method == "POST":
+        print("Request method is POST")
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -47,7 +48,8 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm()
-
+    print("About to render template")
+    
     return render(
         request,
         "blog/post_detail.html",
